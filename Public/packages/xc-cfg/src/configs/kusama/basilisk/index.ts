da5c69375ba58a06a6a60f4fc030f2d00df83989
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 12:43:46 +0100

    chore: add Public/packages/xc-cfg/src/configs/kusama/basilisk/index.ts

---
+++ Public/packages/xc-cfg/src/configs/kusama/basilisk/index.ts
@@ -0,0 +1,65 @@
+import { AssetRoute, ChainRoutes } from '@galacticcouncil/xc-core';
+
+import { ksm, usdt } from '../../../assets';
+import { basilisk, kusamaAssetHub } from '../../../chains';
+import {
+  ExtrinsicBuilder,
+  FeeAmountBuilder,
+  XcmTransferType,
+} from '../../../builders';
+
+import { balance, fee } from './configs';
+
+const toAssetHub: AssetRoute[] = [
+  new AssetRoute({
+    source: {
+      asset: usdt,
+      balance: balance(),
+      fee: fee(),
+      destinationFee: {
+        balance: balance(),
+      },
+    },
+    destination: {
+      chain: kusamaAssetHub,
+      asset: usdt,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: usdt,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+];
+
+const toKusamaAssetHub: AssetRoute[] = [
+  // Basilisk -> Kusama Asset Hub: DestinationReserve with DepositAsset
+  new AssetRoute({
+    source: {
+      asset: ksm,
+      balance: balance(),
+      fee: fee(),
+      destinationFee: {
+        balance: balance(),
+      },
+    },
+    destination: {
+      chain: kusamaAssetHub,
+      asset: ksm,
+      fee: {
+        amount: 0.0012,
+        asset: ksm,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().transferAssetsUsingTypeAndThen({
+      transferType: XcmTransferType.DestinationReserve,
+    }),
+  }),
+];
+
+export const basiliskConfig = new ChainRoutes({
+  chain: basilisk,
+  routes: [...toAssetHub, ...toKusamaAssetHub],
+});
