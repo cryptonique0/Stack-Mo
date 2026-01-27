53150abf3a8fe883bd83b54bac4acd6f7dabb0dc
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 12:43:46 +0100

    chore: add Public/packages/xc-cfg/src/configs/kusama/index.ts

---
+++ Public/packages/xc-cfg/src/configs/kusama/index.ts
@@ -0,0 +1,9 @@
+import { ChainRoutes } from '@galacticcouncil/xc-core';
+
+import { assetHubConfig } from './assethub';
+import { basiliskConfig } from './basilisk';
+
+export const kusamaChainsConfig: ChainRoutes[] = [
+  assetHubConfig,
+  basiliskConfig,
+];
