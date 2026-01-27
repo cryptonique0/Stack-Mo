1f18c3259cee5f92809d33bfe9ee40e0743172c9
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 08:16:55 +0100

    chore: add Public/Folder/packages/sdk-next/test/script/const.ts

---
+++ Public/Folder/packages/sdk-next/test/script/const.ts
@@ -0,0 +1,8 @@
+const TRSRY_ACC = '7L53bUTBopuwFt3mKUfmkzgGLayYa1Yvn1hAg9v5UMrQzTfh';
+const ALICE_ACC = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+
+export const BENEFICIARY = TRSRY_ACC;
+
+export const MAX_RETRIES = 3;
+export const HOUR_MS = 60 * 60 * 1000;
+export const DAY_MS = 1 * 24 * HOUR_MS;
