6f788fc9bc6fa1be6f7648689be1404184792053
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 08:20:19 +0100

    chore: add Public/packages/sdk-next/test/script/examples/aave/getMaxWithdraw.ts

---
+++ Public/packages/sdk-next/test/script/examples/aave/getMaxWithdraw.ts
@@ -0,0 +1,25 @@
+import { PolkadotClient } from 'polkadot-api';
+
+import { createSdkContext, big } from '../../../../src';
+
+import { PapiExecutor } from '../../PapiExecutor';
+import { BENEFICIARY } from '../../const';
+import { ApiUrl } from '../../types';
+
+class GetMaxWithdraw extends PapiExecutor {
+  async script(client: PolkadotClient) {
+    const sdk = await createSdkContext(client);
+
+    const { api } = sdk;
+
+    const { amount, decimals } = await api.aave.getMaxWithdraw(BENEFICIARY, 15);
+    console.log(big.toDecimal(amount, decimals));
+
+    return () => {
+      sdk.destroy();
+      client.destroy();
+    };
+  }
+}
+
+new GetMaxWithdraw(ApiUrl.Hydration, 'Get max withdraw').run();
