bfd0b274222613acb115a2f61077993cdbbe9360
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 08:16:54 +0100

    chore: add Public/Folder/packages/sdk-next/src/types.ts

---
+++ Public/Folder/packages/sdk-next/src/types.ts
@@ -0,0 +1,56 @@
+import { XcmV3Junctions } from '@galacticcouncil/descriptors';
+
+export type Amount = {
+  amount: bigint;
+  decimals: number;
+};
+
+export interface AssetBalance {
+  id: number;
+  balance: Balance;
+}
+
+export interface AssetMetadata {
+  decimals: number;
+  symbol: string;
+}
+
+export type AssetType = 'StableSwap' | 'Bond' | 'Token' | 'External' | 'Erc20';
+
+export interface Asset extends AssetMetadata {
+  id: number;
+  name: string;
+  icon: string;
+  type: AssetType;
+  existentialDeposit: bigint;
+  isSufficient: boolean;
+  location?: XcmV3Multilocation;
+  meta?: Record<string, string>;
+  isWhiteListed?: boolean;
+}
+
+export interface Bond extends Asset {
+  underlyingAssetId: number;
+  maturity: number;
+}
+
+export interface ExternalAsset extends AssetMetadata {
+  id: string;
+  origin: number;
+  name: string;
+  internalId: number;
+  isWhiteListed?: boolean;
+}
+
+export interface Balance {
+  free: bigint;
+  total: bigint;
+  transferable: bigint;
+  reserved: bigint;
+  frozen: bigint;
+}
+
+export type XcmV3Multilocation = {
+  parents: number;
+  interior: XcmV3Junctions;
+};
