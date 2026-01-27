04e4cf64464efd73d23183321d0e5e004d638ebe
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 12:44:00 +0100

    chore: add Public/packages/xcm-cfg/src/builders/extrinsics/xcm/index.ts

---
+++ Public/packages/xcm-cfg/src/builders/extrinsics/xcm/index.ts
@@ -0,0 +1,9 @@
+export { ethereumXcm } from './ethereumXcm';
+export { polkadotXcm } from './polkadotXcm';
+export { tokenGateway } from './tokenGateway';
+export { xcmPallet } from './xcmPallet';
+export { xTokens } from './/xTokens';
+export { xTransfer } from './/xTransfer';
+
+export * from './builder';
+export * from './types';
