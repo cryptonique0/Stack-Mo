c752b65c74a01b7b68ee9be7a91a29050bcdcc8d
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 12:43:47 +0100

    chore: add Public/packages/xc-cfg/src/configs/polkadot/bifrost.ts

---
+++ Public/packages/xc-cfg/src/configs/polkadot/bifrost.ts
@@ -0,0 +1,293 @@
+import { AssetRoute, ChainRoutes } from '@galacticcouncil/xc-core';
+
+import {
+  bnc,
+  dot,
+  usdc,
+  usdt,
+  vdot,
+  vastr,
+  astr,
+  glmr,
+  ibtc,
+} from '../../assets';
+import { assetHub, bifrost, hydration, polkadot } from '../../chains';
+import {
+  BalanceBuilder,
+  ExtrinsicBuilder,
+  FeeAmountBuilder,
+  XcmTransferType,
+} from '../../builders';
+
+const toHydration: AssetRoute[] = [
+  new AssetRoute({
+    source: {
+      asset: bnc,
+      balance: BalanceBuilder().substrate().system().account(),
+      destinationFee: {
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: bnc,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: bnc,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: vdot,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: vdot,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: vdot,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: vastr,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: vastr,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: vastr,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: dot,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: dot,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee({ reserve: polkadot }),
+        asset: dot,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().transferAssetsUsingTypeAndThen({
+      transferType: XcmTransferType.RemoteReserve,
+    }),
+  }),
+  new AssetRoute({
+    source: {
+      asset: astr,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: astr,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: astr,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: glmr,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: glmr,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: glmr,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: ibtc,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: ibtc,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: ibtc,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: usdt,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: usdt,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee({ reserve: assetHub }),
+        asset: usdt,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: usdc,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: usdc,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee({ reserve: assetHub }),
+        asset: usdc,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+];
+
+const toAssetHub: AssetRoute[] = [
+  new AssetRoute({
+    source: {
+      asset: usdt,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: assetHub,
+      asset: usdt,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: usdt,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+  new AssetRoute({
+    source: {
+      asset: usdc,
+      balance: BalanceBuilder().substrate().tokens().accounts(),
+      fee: {
+        asset: bnc,
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+      destinationFee: {
+        balance: BalanceBuilder().substrate().tokens().accounts(),
+      },
+    },
+    destination: {
+      chain: assetHub,
+      asset: usdc,
+      fee: {
+        amount: FeeAmountBuilder()
+          .XcmPaymentApi()
+          .calculateDestFee(),
+        asset: usdc,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+];
+
+export const bifrostConfig = new ChainRoutes({
+  chain: bifrost,
+  routes: [...toHydration, ...toAssetHub],
+});
