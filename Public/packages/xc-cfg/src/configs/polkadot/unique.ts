86f0965109f862b9a62afb1575916714940171b5
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 12:43:48 +0100

    chore: add Public/packages/xc-cfg/src/configs/polkadot/unique.ts

---
+++ Public/packages/xc-cfg/src/configs/polkadot/unique.ts
@@ -0,0 +1,35 @@
+import { AssetRoute, ChainRoutes } from '@galacticcouncil/xc-core';
+
+import { unq } from '../../assets';
+import { hydration, unique } from '../../chains';
+import {
+  BalanceBuilder,
+  ExtrinsicBuilder,
+  FeeAmountBuilder,
+} from '../../builders';
+
+const toHydration: AssetRoute[] = [
+  new AssetRoute({
+    source: {
+      asset: unq,
+      balance: BalanceBuilder().substrate().system().account(),
+      destinationFee: {
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: unq,
+      fee: {
+        amount: FeeAmountBuilder().XcmPaymentApi().calculateDestFee(),
+        asset: unq,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+];
+
+export const uniqueConfig = new ChainRoutes({
+  chain: unique,
+  routes: [...toHydration],
+});
