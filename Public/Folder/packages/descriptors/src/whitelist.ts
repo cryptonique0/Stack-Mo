7424f92584b54788e1034d85551b36a9ef35b3c2
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 08:16:48 +0100

    chore: add Public/Folder/packages/descriptors/src/whitelist.ts

---
+++ Public/Folder/packages/descriptors/src/whitelist.ts
@@ -0,0 +1,68 @@
+import {
+  HydrationWhitelistEntry,
+  HubWhitelistEntry,
+  WhitelistEntriesByChain,
+} from '../.papi/descriptors';
+
+const hydrationWhitelist: HydrationWhitelistEntry[] = [
+  // runtime
+  'api.AaveTradeExecutor.*',
+  'api.CurrenciesApi.*',
+  'api.EthereumRuntimeRPCApi.*',
+  // const
+  'const.Aura.*',
+  'const.DCA.*',
+  'const.DynamicFees.*',
+  'const.HSM.*',
+  'const.LBP.*',
+  'const.Omnipool.*',
+  'const.Stableswap.*',
+  'const.XYK.*',
+  'const.Staking.*',
+  // event
+  'event.EVM.Log',
+  'event.Router.Executed',
+  // query
+  'query.AssetRegistry.*',
+  'query.Bonds.*',
+  'query.DynamicFees.*',
+  'query.EmaOracle.*',
+  'query.Ethereum.*',
+  'query.HSM.*',
+  'query.LBP.*',
+  'query.MultiTransactionPayment.*',
+  'query.Omnipool.*',
+  'query.OmnipoolWarehouseLM.*',
+  'query.ParachainSystem.ValidationData',
+  'query.Referenda.*',
+  'query.Tokens.*',
+  'query.Stableswap.*',
+  'query.Staking.*',
+  'query.Uniques.Account',
+  'query.XYK.*',
+  'query.XYKWarehouseLM.*',
+  // tx
+  'tx.DCA.*',
+  'tx.Dispatcher.*',
+  'tx.Omnipool.*',
+  'tx.Router.*',
+];
+
+const hubWhitelist: HubWhitelistEntry[] = [
+  'query.ForeignAssets.*',
+  'query.Assets.*',
+  'api.AssetConversionApi.*',
+  'tx.Assets.*',
+];
+
+export const whitelist: WhitelistEntriesByChain = {
+  '*': [
+    'api.DryRunApi.*',
+    'api.XcmPaymentApi.*',
+    'const.System.*',
+    'query.System.*',
+    'tx.PolkadotXcm.*',
+  ],
+  hydration: hydrationWhitelist,
+  hub: hubWhitelist,
+};
