df74997490d0de5a9ce2ec7f59f06f1741a16247
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 08:20:19 +0100

    chore: add Public/packages/sdk-next/test/script/PapiExecutor.ts

---
+++ Public/packages/sdk-next/test/script/PapiExecutor.ts
@@ -0,0 +1,68 @@
+import { PolkadotClient, createClient } from 'polkadot-api';
+import { hydration } from '@galacticcouncil/descriptors';
+
+import { ApiUrl } from './types';
+
+import { api, json, evm } from '../../src';
+
+export abstract class PapiExecutor {
+  protected readonly apiUrl: ApiUrl;
+  protected readonly desc: string;
+  protected readonly pretty: boolean;
+
+  constructor(apiUrl: ApiUrl, desc: string, pretty?: boolean) {
+    this.apiUrl = apiUrl;
+    this.desc = desc;
+    this.pretty = pretty || false;
+  }
+
+  async run() {
+    console.log('Connecting to:', this.apiUrl);
+    const provider = api.getWs(this.apiUrl);
+    const client = createClient(provider);
+    const papi = client.getTypedApi(hydration);
+    const { spec_name, spec_version } = await papi.constants.System.Version();
+    console.log(`Runtime ready ${spec_name}/${spec_version}`);
+    console.log('Running script...');
+    console.log(this.desc);
+    console.time('Execution time:');
+
+    const evmClient = new evm.EvmClient(client);
+
+    this.script(client, evmClient)
+      .then((output: any) => {
+        if (typeof output === 'function') {
+          setTimeout(output, 10_000);
+          return;
+        }
+
+        if (this.pretty) {
+          console.log(
+            output ? JSON.stringify(output, json.jsonFormatter, 2) : ''
+          );
+        } else {
+          console.log(output);
+        }
+        console.log('Destroying...');
+        client.destroy();
+      })
+      .catch((e) => {
+        console.log(e);
+        client.destroy();
+      })
+      .finally(() => {
+        console.timeEnd('Execution time:');
+      });
+  }
+
+  abstract script(client: PolkadotClient, evm: evm.EvmClient): Promise<any>;
+
+  logTime() {
+    const time = [
+      '-----',
+      new Date().toISOString().replace('T', ' ').replace('Z', ''),
+      '-----',
+    ].join('');
+    console.log(time);
+  }
+}
