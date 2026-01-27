568e43c6115d99a3593fc1ae1559b53cb9cf0eb5
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-27 12:43:48 +0100

    chore: add Public/packages/xc-cfg/src/configs/polkadot/pendulum.ts

---
+++ Public/packages/xc-cfg/src/configs/polkadot/pendulum.ts
@@ -0,0 +1,35 @@
+import { AssetRoute, ChainRoutes } from '@galacticcouncil/xc-core';
+
+import { pen } from '../../assets';
+import { hydration, pendulum } from '../../chains';
+import {
+  BalanceBuilder,
+  ExtrinsicBuilder,
+  FeeAmountBuilder,
+} from '../../builders';
+
+const toHydration: AssetRoute[] = [
+  new AssetRoute({
+    source: {
+      asset: pen,
+      balance: BalanceBuilder().substrate().system().account(),
+      destinationFee: {
+        balance: BalanceBuilder().substrate().system().account(),
+      },
+    },
+    destination: {
+      chain: hydration,
+      asset: pen,
+      fee: {
+        amount: FeeAmountBuilder().XcmPaymentApi().calculateDestFee(),
+        asset: pen,
+      },
+    },
+    extrinsic: ExtrinsicBuilder().polkadotXcm().limitedReserveTransferAssets(),
+  }),
+];
+
+export const pendulumConfig = new ChainRoutes({
+  chain: pendulum,
+  routes: [...toHydration],
+});
